f9556a22ab860db7907b0eca6b5a58f0c72faa5a
Pxlsnt <664230044@webmail.npru.ac.th>
2025-07-22 21:32:07 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/submit", (req, res) => {
+  const data = req.body;
+  console.log("ได้รับข้อมูลจากแบบฟอร์ม:", data);
+
+  fs.appendFile("log.json", JSON.stringify(data) + ",\n", (err) => {
+    if (err) {
+      res.status(500).send("บันทึกไม่สำเร็จ");
+    } else {
+      res.send("บันทึกเรียบร้อย");
+    }
+  });
+});
+
+app.listen(3000, () => console.log("เซิร์ฟเวอร์จำลองเปิดที่ http://localhost:3000"));
